263fa2dc7eef139581a2817c5c37f90974d05d5a
qq389146213 <389146213@qq.com>
2018-05-23 23:41:02 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,9 @@
-if [ ! -d /v2raybin/v2ray-v$V2RAY_VER-linux-64 ]; then
-  rm -rf /v2raybin
-  mkdir /v2raybin
-  cd /v2raybin
-  wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$V2RAY_VER/v2ray-linux-64.zip
-  unzip v2ray.zip 
-  cd /v2raybin/v2ray-v$V2RAY_VER-linux-64
-  chmod +x v2ray
-  chmod +x v2ctl
-fi
-
 if [ ! -d /caddybin/caddy_v$CADDY_VER ]; then
   rm -rf /caddybin
   mkdir /caddybin
   mkdir /caddybin/caddy_v$CADDY_VER
   cd /caddybin/caddy_v$CADDY_VER
-  wget -O caddy.tar.gz https://github.com/mholt/caddy/releases/download/v$CADDY_VER/caddy_v$CADDY_VER'_linux_amd64.tar.gz'
+  wget -O caddy.tar.gz 'https://caddyserver.com/download/linux/amd64?plugins=http.forwardproxy&license=personal'
   tar xvf caddy.tar.gz 
   chmod +x caddy
 fi
@@ -26,23 +15,13 @@ if [ ! -d $CADDY_ROOT ]; then
   unzip wallet.bitshares.org-gh-pages.zip
 fi
 
-cd /v2raybin/v2ray-v$V2RAY_VER-linux-64
-echo -e -n "$CONFIG_JSON1" > config.json
-echo -e -n "$V2_WS_PORT,\"listen\":\"$V2_WS_IP\"" >> config.json
-echo -e -n "$CONFIG_JSON2" >> config.json
-echo -e -n "$UUID" >> config.json
-echo -e -n "$CONFIG_JSON3" >> config.json
-echo -e -n "$V2_WS_PATH" >> config.json
-echo -e -n "$CONFIG_JSON4" >> config.json
-./v2ray &
-
 cd /caddybin/caddy_v$CADDY_VER
 echo $CADDY_LS_IP:$PORT { > HerokuCaddyfile
 echo root $CADDY_ROOT/wallet.bitshares.org-gh-pages >> HerokuCaddyfile
 echo gzip >> HerokuCaddyfile
 echo index $CADDY_INDEX >> HerokuCaddyfile
-echo proxy $V2_WS_PATH $V2_WS_IP:$V2_WS_PORT { >> HerokuCaddyfile
-echo websocket >> HerokuCaddyfile
+echo forwardproxy { >> HerokuCaddyfile
+echo basicauth $CADDY_H2_USER $CADDY_H2_PWD >> HerokuCaddyfile
 echo } >> HerokuCaddyfile
 echo } >> HerokuCaddyfile
 ./caddy -conf="HerokuCaddyfile"
